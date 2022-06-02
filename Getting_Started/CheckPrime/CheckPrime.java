ebc5249352442a22c10b1eed6f4b7178a2522837
mayurpothuri <mayurpothuri@gmail.com>
2022-06-02 18:18:36 +0530

    added efficient implementation for checking prime which saves many iterations but the time complexity is roughly sqrt(n)

--- Getting_Started/CheckPrime/CheckPrime.java
+++ Getting_Started/CheckPrime/CheckPrime.java
@@ -2,8 +2,29 @@ package Getting_Started.CheckPrime;
 import java.util.*;
 
 public class CheckPrime {
+    //this more improved version than O(sqrt(n))
+    //we are iterating by 6[as lcm(2,3) = 6 also we are checking mod 2 and mod 3] jumps.
+    //while jumping from i = 5, 5 + 6[11], 5 + 6 + 6[17], ... we also need to 7[5 + 2], 5 + 6 + 2[13], ....
+    //hence in the loop, need to check n%i == 0 && n%(i + 2) == 0
+    public static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        else if(n == 2 || n == 3){
+            return true;
+        }
+        for(int i = 5; i*i <= n; i += 6){
+            if(n%i == 0 || n%(i + 2) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
     //time complexity = O(sqrt(n))
     public static boolean checkPrime(int n) {
+        if(n == 1){
+            return false;
+        }
         if(n == 2 || n == 3){
             return true;
         }
@@ -20,7 +41,7 @@ public class CheckPrime {
         int n;
         for(int i = 0; i < t; i++){
             n = scanner.nextInt();
-            if(checkPrime(n)){
+            if(isPrime(n)){
                 System.out.println("prime");
             }
             else{
