de7d180b49b9a26668a0c3f1dfd2a1caea26a559
mayurpothuri <mayurpothuri@gmail.com>
2022-06-04 14:11:14 +0530

    added program for pattern_2.

--- Patterns/Pattern_2/Pattern2.java
+++ Patterns/Pattern_2/Pattern2.java
@@ -1,5 +1,23 @@
 package Patterns.Pattern_2;
+import java.util.*;
 
 public class Pattern2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        printPattern(n);
+        scanner.close();
+        return;
+    }
+
+    private static void printPattern(int n) {
+
+        for(int i = 0; i < n; i++){
+            for(int j = n - i; j > 0; j--){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+        return;
+    }
 }
