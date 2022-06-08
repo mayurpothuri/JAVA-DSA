c98d33aa8de746b5dbd62e27572043e23154ed72
mayurpothuri <mayurpothuri@gmail.com>
2022-06-08 16:58:40 +0530

    added program to n to 1 using recursion for given number n along with time complexity and added pre area and post area for recursion concept.

--- Recursion_and_Backtracking/PrintDecreasing/PrintDecreasing.java
+++ Recursion_and_Backtracking/PrintDecreasing/PrintDecreasing.java
@@ -1,26 +1,27 @@
-package Recursion_and_Backtracking.PrintIncreasing;
+package Recursion_and_Backtracking.PrintDecreasing;
 
 import java.util.Scanner;
 
-public class PrintIncreasing {
+public class PrintDecreasing {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        printIncreasing(n);
+        int n =  scanner.nextInt();
+
+        printDecreasing(n);
         scanner.close();
         return;
     }
 
-    //print 1 to n using recursion.
+    //print n to 1 using recursion.
     //time complexity - O(n)
-    private static void printIncreasing(int n) {
+    private static void printDecreasing(int n) {
         if(n <= 0){
             return;
         }
-        printIncreasing(n - 1);
-        //post area
+        //pre area
         System.out.println(n);
+        printDecreasing(n - 1);
         return;
     }
 }
