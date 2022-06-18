8953b6483cbe96a3c8f226445b8c5b44891afeea
mayurpothuri <mayurpothuri@gmail.com>
2022-06-18 14:01:09 +0530

    added program Dynamic Stack implementation and also adjusted 'this' argument in Normal Stack implementation program.

--- Stack_and_Queue/DynamicStack/CustomStack.java
+++ Stack_and_Queue/DynamicStack/CustomStack.java
@@ -1,32 +1,36 @@
-package Stack_and_Queue.NormalStack;
+package Stack_and_Queue.DynamicStack;
 
 public class CustomStack {
-    int tos; // keeps track of top element on stack
-    int[] data;// elements storage for stack
+
+    int tos;
+    int[] data;
 
     CustomStack(int n) {
         this.tos = -1;
         this.data = new int[n];
     }
 
-    // if tos + 1 == capacity of "data" array then Stack Overflow
     public void push(int val) {
         if (this.tos + 1 == this.data.length) {
-            System.out.println("Stack overflow");
+            int[] newData = new int[2 * this.data.length];
+            int i = 0;
+            for (; i < this.data.length; i++) {
+                newData[i] = this.data[i];
+            }
+            newData[i] = val;
+            this.tos = i;
+            this.data = newData;
         } else {
             this.data[++this.tos] = val;
         }
         return;
     }
 
-    // if tos == -1 then Stack underflow, as the stack is empty and nothing to pop
-    // out.
     public int pop() {
         if (this.tos == -1) {
             System.out.println("Stack underflow");
         } else {
-            int val = this.data[this.tos--];
-            return val;
+            return this.data[this.tos--];
         }
         return -1;
     }
@@ -40,12 +44,10 @@ public class CustomStack {
         return -1;
     }
 
-    // tos + 1 gives the size of stack as 0 - based counting is there
     public int size() {
         return this.tos + 1;
     }
 
-    // from end to start of "data" array, print the elements
     public void display() {
         for (int i = this.size() - 1; i >= 0; i--) {
             System.out.print(this.data[i] + " ");
